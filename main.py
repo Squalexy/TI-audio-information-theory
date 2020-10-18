6b19675a796c7b9eaabe8b80b010174b328650e7
Sofia Neves <sofia.neves285@gmail.com>
2020-10-18 14:47:09 +0100

    entropia

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import scipy.io.wavfile as spiowf
-import sounddevice as sd
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
@@ -26,6 +25,7 @@ def texto(data):
             print(f"Letra : {caracter}, {ocorrencias[numero_caracter]}")
 
     histograma(alfabeto, ocorrencias)
+    return ocorrencias
 
 
 def img_audio(data):
@@ -43,6 +43,7 @@ def img_audio(data):
         ocorrencias[element] += 1
 
     histograma(alfabeto, ocorrencias)
+    return ocorrencias
 
 
 def histograma(alfabeto, ocorrencias):
@@ -51,24 +52,47 @@ def histograma(alfabeto, ocorrencias):
     plt.show()
 
 
+def entropia(ocorrencias):
+    entropia = 0
+    probabilidade = np.zeros(len(ocorrencias))
+
+    for i in range(len(ocorrencias)):
+        probabilidade[i] = ocorrencias[i] / sum(ocorrencias)
+
+        if probabilidade[i] != 0:
+            entropia += (probabilidade[i] * np.log2(probabilidade[i]))
+
+    return -entropia
+
+
 def main(filename):
-    if filename.lower().endswith(('.png', '.jpg', '.jpeg','.bmp')):  # Se o ficheiro tiver uma destas extenções, significa que corresponde a uma imagem
+    if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp')):  # Se o ficheiro tiver uma destas extenções, significa que corresponde a uma imagem
         data = mpimg.imread(filename)
-        img_audio(data)
+        if data.ndim == 4:
+            ocorrencias = img_audio(data[:, :, :, 0])
+        else:
+            ocorrencias = img_audio(data)
 
     elif filename.lower().endswith(('.wav', '.mp3')):
-        [samplerate, data] = spiowf.read(filename)
-        img_audio(data)
+        [sampleRate, data] = spiowf.read(filename)
+        if data.ndim == 2:
+            ocorrencias = img_audio(data[:, 0])
+        else:
+            ocorrencias = img_audio(data)
 
     elif filename.lower().endswith('.txt'):
         with open(filename) as f:
-            data = list(f.read())
-        texto(data)
+            data = np.asarray(list(f.read()))
+
+        ocorrencias = texto(data)
+
+    print(f"\nENTROPIA:\n {entropia(ocorrencias)}")
 
 
 if __name__ == "__main__":
-    main("lena.bmp")
-    # main("binaria.bmp")
-    # main("ct1.bmp")
-    main("texto.txt")
-    # main("Song01.wav")
+    #main("lena.bmp")
+    main("binaria.bmp")
+    #main("ct1.bmp")
+    #main("texto.txt")
+    #main("Song01.wav")
+    #main("Saxriff.wav")
